fd74187d56989113a6cdf0f3fcf852175c9dcaec
Steven Ewald <stevenewald6@gmail.com>
2025-07-17 12:48:43 -0500

    Initial string support added

--- include/gloss/gloss.hpp
+++ include/gloss/gloss.hpp
@@ -7,9 +7,12 @@
 #include <array>
 #include <bit>
 #include <ranges>
+#include <string>
 
 namespace gloss {
 
+using u8 = std::uint8_t;
+using u16 = std::uint16_t;
 using u32 = std::uint32_t;
 using u64 = std::uint64_t;
 
@@ -33,6 +36,133 @@ private:
 };
 } // namespace random
 
+template <typename T>
+constexpr T
+to(const auto& data)
+{
+    // using data_type = decltype(data);
+    if constexpr (requires { data.size(); }) {
+        T tmp{};
+        for (std::size_t i = 0; i < data.size(); ++i) {
+            tmp |= static_cast<T>(static_cast<T>(data[i]) << (i * __CHAR_BIT__));
+        }
+        return tmp;
+    }
+    else if constexpr (requires(u32 n) { data[n]; }) {
+        T tmp{};
+        for (std::size_t i = 0; i < sizeof(T) && data[i] != '\0'; ++i) {
+            tmp |= static_cast<T>(static_cast<T>(data[i]) << (i * __CHAR_BIT__));
+        }
+        return tmp;
+    }
+    else if constexpr (std::same_as<T, std::string>) {
+        std::size_t str_size{};
+        std::string chars(sizeof(decltype(data)), '\0');
+        for (std::size_t i = 0; i < chars.size(); ++i) {
+            chars[i] = static_cast<char>(data >> (i * __CHAR_BIT__));
+            if ((data >> (i * __CHAR_BIT__)) != 0) {
+                str_size = i + 1;
+            }
+        }
+        chars.resize(str_size);
+        return chars;
+    }
+    else {
+        return static_cast<T>(data);
+    }
+}
+
+template <const auto& Table>
+struct entries {
+    using pair_type = std::ranges::range_value_t<decltype(Table)>;
+    static constexpr auto SIZE = Table.size();
+
+    // Support string_view, const char*, and integral keys
+    using key_type = decltype([]() {
+        if constexpr (requires { Table[0].first.size(); }) {
+            static constexpr auto MAX_SIZE = []() {
+                std::size_t max{};
+                for (std::size_t i = 0; i < SIZE; ++i) {
+                    max = std::max(max, Table[i].first.size());
+                }
+                return max;
+            }();
+
+            if constexpr (MAX_SIZE <= sizeof(u8))
+                return u8{};
+            else if constexpr (MAX_SIZE <= sizeof(u16))
+                return u16{};
+            else if constexpr (MAX_SIZE <= sizeof(u32))
+                return u32{};
+            else if constexpr (MAX_SIZE <= sizeof(u64))
+                return u64{};
+        }
+        else if constexpr (std::is_same_v<
+                               typename pair_type::first_type, const char*>) {
+            static constexpr auto MAX_SIZE = []() {
+                std::size_t max{};
+                for (std::size_t i = 0; i < SIZE; ++i) {
+                    const char* tmp = Table[i].first;
+                    std::size_t len{};
+                    while (*tmp != '\0') {
+                        ++tmp;
+                        ++len;
+                    }
+                    max = std::max(max, len);
+                }
+                return max;
+            }();
+
+            if constexpr (MAX_SIZE <= sizeof(u8))
+                return u8{};
+            else if constexpr (MAX_SIZE <= sizeof(u16))
+                return u16{};
+            else if constexpr (MAX_SIZE <= sizeof(u32))
+                return u32{};
+            else if constexpr (MAX_SIZE <= sizeof(u64))
+                return u64{};
+        }
+        else {
+            return typename pair_type::first_type{};
+        }
+    }());
+
+    // Support string and integral values
+    using mapped_type = decltype([]() {
+        if constexpr (std::is_same_v<typename pair_type::second_type, std::string>) {
+            static constexpr auto MAX_SIZE = []() {
+                std::size_t max{};
+                for (std::size_t i = 0; i < SIZE; ++i) {
+                    max = std::max(max, Table[i].second.size());
+                }
+                return max;
+            }();
+
+            if constexpr (MAX_SIZE <= sizeof(u8))
+                return u8{};
+            else if constexpr (MAX_SIZE <= sizeof(u16))
+                return u16{};
+            else if constexpr (MAX_SIZE <= sizeof(u32))
+                return u32{};
+            else if constexpr (MAX_SIZE <= sizeof(u64))
+                return u64{};
+        }
+        else {
+            return typename pair_type::second_type{};
+        }
+    }());
+
+    static constexpr auto MAPPINGS = []() {
+        std::array<std::pair<key_type, mapped_type>, SIZE> entries;
+        for (std::size_t i = 0; i < SIZE; ++i) {
+            entries[i] = std::pair<key_type, mapped_type>{
+                to<key_type>(Table[i].first), to<mapped_type>(Table[i].second)
+            };
+        }
+        return entries;
+    }();
+};
+
 template <typename P>
 concept Pair = requires {
     typename P::first_type;
@@ -46,8 +176,9 @@ concept PairRange =
 template <const auto& Table, typename ValueType>
 requires PairRange<decltype(Table)>
 struct lookup_lut {
-    using key_type = std::ranges::range_value_t<decltype(Table)>::first_type;
-    using mapped_type = std::ranges::range_value_t<decltype(Table)>::second_type;
+    using key_type = entries<Table>::key_type;
+    using mapped_type = entries<Table>::mapped_type;
+    using result_type = std::ranges::range_value_t<decltype(Table)>::second_type;
 
     consteval explicit lookup_lut(std::uint32_t max_attempts = 100'000) noexcept
     {
@@ -59,7 +190,7 @@ struct lookup_lut {
 
         auto attempt_find_perfect_hash = [&]() {
             magic_ = rand_pcg();
-            for (const auto& [key, value] : Table) {
+            for (const auto& [key, value] : entries<Table>::MAPPINGS) {
                 u32 shift = u32((key * magic_) >> SHIFT);
                 if (shift >= sizeof(ValueType) * __CHAR_BIT__) {
                     lut_ = {};
@@ -69,7 +200,7 @@ struct lookup_lut {
                 lut_ |= ValueType(value) << shift;
             }
 
-            for (const auto& [key, value] : Table) {
+            for (const auto& [key, value] : entries<Table>::MAPPINGS) {
                 if ((lut_ >> (ValueType(key) * magic_ >> SHIFT) & MASK) != value) {
                     lut_ = {};
                     return;
@@ -88,18 +219,18 @@ struct lookup_lut {
         return MASK and SHIFT and magic_ and lut_;
     }
 
-    constexpr mapped_type
-    operator()(const key_type& search_key) const noexcept
+    constexpr result_type
+    operator()(const auto& search_key) const noexcept
     {
-        return static_cast<mapped_type>(
-            (lut_ >> ((search_key * magic_) >> SHIFT)) & MASK
+        return to<result_type>(
+            (lut_ >> ((to<key_type>(search_key) * magic_) >> SHIFT)) & MASK
         );
     }
 
 private:
     static constexpr ValueType MAX_BITS = []() {
         u32 max = 0;
-        for (const auto& pair : Table) {
+        for (const auto& pair : entries<Table>::MAPPINGS) {
             max = std::max(static_cast<u32>(pair.second), max);
         }
         // std::countl_zero counts differently, so use builtin
@@ -118,8 +249,9 @@ requires PairRange<decltype(Table)>
 struct lookup_array {
     using ValueType = u64;
 
-    using key_type = std::ranges::range_value_t<decltype(Table)>::first_type;
-    using mapped_type = std::ranges::range_value_t<decltype(Table)>::second_type;
+    using key_type = entries<Table>::key_type;
+    using mapped_type = entries<Table>::mapped_type;
+    using result_type = std::ranges::range_value_t<decltype(Table)>::second_type;
 
     consteval explicit lookup_array(std::uint32_t max_attempts = 100'000) noexcept
     {
@@ -127,7 +259,7 @@ struct lookup_array {
 
         auto attempt_find_perfect_hash = [&]() {
             magic_ = rand_pcg();
-            for (const auto& [key, value] : Table) {
+            for (const auto& [key, value] : entries<Table>::MAPPINGS) {
                 u32 shift = u32(((key * magic_) >> SHIFT) & SIZE_MASK);
                 if (shift >= SIZE) {
                     table_ = {};
@@ -135,11 +267,12 @@ struct lookup_array {
                     return;
                 }
 
-                table_[shift] = value;
+                table_[shift] = to<mapped_type>(value);
             }
 
-            for (const auto& [key, value] : Table) {
-                if (table_[(ValueType(key) * magic_ >> SHIFT) & SIZE_MASK] != value) {
+            for (const auto& [key, value] : entries<Table>::MAPPINGS) {
+                if (table_[(ValueType(key) * magic_ >> SHIFT) & SIZE_MASK]
+                    != to<mapped_type>(value)) {
                     table_ = {};
                     magic_ = {};
                     return;
@@ -158,11 +291,11 @@ struct lookup_array {
         return magic_ != 0;
     }
 
-    constexpr mapped_type
-    operator()(const key_type& search_key) const noexcept
+    constexpr result_type
+    operator()(const auto& search_key) const noexcept
     {
-        return static_cast<mapped_type>(
-            table_[((ValueType(search_key) * magic_ >> SHIFT)) & SIZE_MASK]
+        return to<result_type>(
+            table_[((to<key_type>(search_key) * magic_ >> SHIFT)) & SIZE_MASK]
         );
     }
 
@@ -170,7 +303,7 @@ private:
     static constexpr std::size_t SIZE = Table.size();
     static constexpr ValueType MAX_BITS = []() {
         u32 max = 0;
-        for (const auto& pair : Table) {
+        for (const auto& pair : entries<Table>::MAPPINGS) {
             max = std::max(static_cast<u32>(pair.second), max);
         }
         // std::countl_zero counts differently, so use builtin
@@ -182,7 +315,7 @@ private:
         (1u << static_cast<unsigned>(std::bit_width(SIZE - 1))) - 1u;
 
     ValueType magic_{};
-    std::array<key_type, SIZE> table_{};
+    std::array<mapped_type, SIZE> table_{};
 };
 
 enum class LookupMethod : std::uint8_t { word, array, any };
@@ -191,17 +324,18 @@ template <const auto& Table, LookupMethod Method = LookupMethod::any>
 auto
 lookup(const auto& search_key)
 {
-    if constexpr (constexpr lookup_lut<Table, u32> TABLE32{};
-                  Method != LookupMethod::array && TABLE32) {
-        return TABLE32(search_key);
+    if constexpr (Method != LookupMethod::array) {
+        if constexpr (constexpr lookup_lut<Table, u32> TABLE32{}; TABLE32) {
+            return TABLE32(search_key);
+        }
+        else if constexpr (constexpr lookup_lut<Table, u64> TABLE64{}; TABLE64) {
+            return TABLE64(search_key);
+        }
     }
-    else if constexpr (constexpr lookup_lut<Table, u64> TABLE64{};
-                       Method != LookupMethod::array && TABLE64) {
-        return TABLE64(search_key);
-    }
-    else if constexpr (constexpr lookup_array<Table> TABLE_ARRAY{};
-                       Method != LookupMethod::word && TABLE_ARRAY) {
-        return TABLE_ARRAY(search_key);
+    else if constexpr (Method != LookupMethod::word) {
+        if constexpr (constexpr lookup_array<Table> TABLE_ARRAY{}; TABLE_ARRAY) {
+            return TABLE_ARRAY(search_key);
+        }
     }
 }
 
--- test/source/gloss_test.cpp
+++ test/source/gloss_test.cpp
@@ -22,6 +22,48 @@ TEST_CASE("Map int to int", "[library]")
     REQUIRE(lookup<TEST, LookupMethod::word>(7u) == 8);
 }
 
+TEST_CASE("Map string to int", "[library]")
+{
+    static constexpr auto TEST = std::array{
+        std::pair<std::string_view, uint8_t>{"hi",  8},
+        std::pair<std::string_view, uint8_t>{"bye", 6}
+    };
+    REQUIRE(lookup<TEST, LookupMethod::word>("hi") == 8);
+    REQUIRE(lookup<TEST, LookupMethod::word>("bye") == 6);
+}
+
+#ifndef TARGET_OS_X
+TEST_CASE("Map int to string", "[library]")
+{
+    static constexpr auto TEST = std::array{
+        std::pair<uint8_t, std::string>{8, "h"},
+         std::pair<uint8_t, std::string>{6, "b"}
+    };
+    REQUIRE(lookup<TEST, LookupMethod::array>(8) == "h");
+    REQUIRE(lookup<TEST, LookupMethod::word>(6) == "b");
+}
+
+TEST_CASE("Map int to long string", "[library]")
+{
+    static constexpr auto TEST = std::array{
+        std::pair<uint8_t, std::string>{8, "hello!"},
+        std::pair<uint8_t, std::string>{6, "bye!"  }
+    };
+    REQUIRE(lookup<TEST, LookupMethod::array>(8) == "hello!");
+    REQUIRE(lookup<TEST, LookupMethod::array>(6) == "bye!");
+}
+#endif
+
+TEST_CASE("Map const char* to int", "[library]")
+{
+    static constexpr auto TEST = std::array{
+        std::pair<const char*, uint8_t>{"hi",  8},
+        std::pair<const char*, uint8_t>{"bye", 6}
+    };
+    REQUIRE(lookup<TEST, LookupMethod::word>("hi") == 8);
+    REQUIRE(lookup<TEST, LookupMethod::word>("bye") == 6);
+}
+
 TEST_CASE("Map many ints to int", "[library]")
 {
     static constexpr auto TEST = std::array{
@@ -56,4 +98,46 @@ TEST_CASE("Array LUT", "[library]")
     for (std::uint32_t i = 0; i < SIZE; ++i) {
         REQUIRE(gloss::lookup<TEST, LookupMethod::array>(i) == i + 13);
     }
+}
+
+TEST_CASE("to<T>(const char*)", "[library]")
+{
+    const char* str = "h";
+    REQUIRE(gloss::to<uint64_t>(str) == uint8_t{'h'});
+    REQUIRE(gloss::to<uint8_t>(str) == uint8_t{'h'});
+    str = "hi!";
+    REQUIRE(
+        gloss::to<uint64_t>(str)
+        == (uint32_t{'!'} << 16u | uint32_t{'i'} << 8u | uint32_t{'h'})
+    );
+    REQUIRE(gloss::to<uint16_t>(str) == (uint32_t{'i'} << 8u | uint32_t{'h'}));
+    REQUIRE(gloss::to<uint8_t>(str) == uint32_t{'h'});
+}
+
+TEST_CASE("to<T>(std::string)", "[library]")
+{
+    std::string str{"h"};
+    REQUIRE(gloss::to<uint64_t>(str) == uint8_t{'h'});
+    REQUIRE(gloss::to<uint8_t>(str) == uint8_t{'h'});
+    str = "hi!";
+    REQUIRE(
+        gloss::to<uint64_t>(str)
+        == (uint32_t{'!'} << 16u | uint32_t{'i'} << 8u | uint32_t{'h'})
+    );
+    REQUIRE(gloss::to<uint16_t>(str) == (uint32_t{'i'} << 8u | uint32_t{'h'}));
+    REQUIRE(gloss::to<uint8_t>(str) == uint32_t{'h'});
+}
+
+TEST_CASE("to<T>(std::string_view)", "[library]")
+{
+    constexpr std::string_view STR{"h"};
+    static_assert(gloss::to<uint64_t>(STR) == uint8_t{'h'});
+    static_assert(gloss::to<uint8_t>(STR) == uint8_t{'h'});
+    constexpr std::string_view STR2 = "hi!";
+    static_assert(
+        gloss::to<uint64_t>(STR2)
+        == (uint32_t{'!'} << 16u | uint32_t{'i'} << 8u | uint32_t{'h'})
+    );
+    static_assert(gloss::to<uint16_t>(STR2) == (uint32_t{'i'} << 8u | uint32_t{'h'}));
+    static_assert(gloss::to<uint8_t>(STR2) == uint32_t{'h'});
 }
